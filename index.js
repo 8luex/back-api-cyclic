5dee1bd639d305fdec0d06b7a4a35e2be13a11dc
blubyu <ramida470@gmail.com>
2023-05-18 20:30:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/teachercreated/:teacherID', (req, res) => {
     )
 }) //teacher created
 
-app.get('/teacheredit/:teacherID/:activityID', (req, res) => {
+app.get('/teacheredit/:teacherID=:activityID', (req, res) => {
     let teacherID = req.params.teacherID
     connection.query(
         'SELECT activity.id,activity.creator,activity.name,activity.detail,activity.createdAt,activity.location,activity.eventDate,activity.timeStart,activity.timeEnd,activity.hoursToReceive,activity.image,activity.year,activity.semester,activity.max,teacher.fname AS teacherfname,teacher.lname AS teacherlname,faculty.name AS faculty FROM `activity` JOIN teacher ON creator = teacher.id JOIN faculty ON faculty.id = teacher.faculty WHERE activity.creator = ? AND activity.id = ?;',
